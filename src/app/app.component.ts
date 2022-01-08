e174afafdd8c8a930bb9165ae67a0efb0211dc00
Victor <victorsmirnov@me.com>
2022-01-08 16:59:39 +0200

    create app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ngx-datatable';
+  rows = [
+    {
+      name: "Mercy", age: 10, town: "Nairobi", country: "Kenya"
+    },
+    {
+      name: "Vincent", age: 40, town: "Kampala", country: "Uganda"
+    },
+    {
+      name: "Wesley", age: 41, town: "Cairo", country: "Egypt"
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxDatatableModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
